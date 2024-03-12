b0a9adc7a12db23c1425e64db3508fff3f66fc0c
Andres <aaplv2@gmail.com>
2024-03-12 11:08:37 -0300

    diseño responsivo

---
+++ index.js
@@ -0,0 +1,5 @@
+let content = document.querySelector(".content");
+let editForm = content.querySelector(".popup__container");
+let editButton = content.querySelectorAll(".profile__info-button-edit");
+
+function renderEdit() {}
