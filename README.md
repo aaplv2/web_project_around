3385fe792780b6ec9e288affa8188fdd7a173317
Andres <aaplv2@gmail.com>
2024-03-26 18:14:45 -0300

    1er envio a revision

--- README.md
+++ README.md
@@ -2,9 +2,17 @@
 
 # Perfil de usuario para personas haciendo viajes alrededor del mundo y Estados Unidos.
 
+# Permite editar nombre y caracteristicas del perfil de usuario.
+
+# Permite añadir cartas personalizadas con el título y fotografía que desee el usuario.
+
+# Las cartas pueden inspeccionarse a un zoom mas alto.
+
+# Las cartas pueden recibir reacciones me gusta y ser eliminadas por los botones en ellas mismas.
+
 # HTML, CSS y JavaScript
 
-# Figma
+# Diseño en base a Figma
 
 # Git Bash para pull original y push en rama develop, antes de enviar a revisión en rama main.
 
@@ -16,8 +24,8 @@
 
 # Metodología BEM y semántica correspondiente para los elementos
 
-# Diseño en base a herramientas flexbox para orden.
+# Diseño en base a herramientas flexbox y grid para orden.
 
-# Javascrip con declaraciones variables, constanstes y funciones. Permitiendo al usuario interactuar con el boton edit.
+# Javascrip con declaraciones variables, constanstes y funciones. Permitiendo al usuario interactuar con el boton edit, añadir nueva carta, me gusta, eliminar y la misma fotografía.
 
 # GitHub Pages: https://aaplv2.github.io/web_project_around/
--- scripts/index.js
+++ scripts/index.js
@@ -1,25 +1,117 @@
-let content = document.querySelector(".body");
-let editPopout = content.querySelector(".popout");
-let overlayPopout = content.querySelector("#overlay");
-let editButton = content.querySelector(".profile__button-edit");
-let closeButton = content.querySelector(".popout__button-close");
-let formElement = content.querySelector(".popout__form");
-let saveButton = content.querySelector(".popout__button-save");
+const content = document.querySelector(".body");
+
+const addButton = content.querySelector(".profile__button-add");
+const addFormPopout = content.querySelector(".popout-add");
+const addCloseButton = content.querySelector(".popout-add__button-close");
+const addFormElement = content.querySelector(".popout-add__form");
+const addSaveButton = content.querySelector(".popout-add__button-save");
+
+const editButton = content.querySelector(".profile__button-edit");
+const editFormPopout = content.querySelector(".popout-edit");
+const editCloseButton = content.querySelector(".popout-edit__button-close");
+const editFormElement = content.querySelector(".popout-edit__form");
+const editSaveButton = content.querySelector(".popout-edit__button-save");
+
+const imagePopout = content.querySelector(".popout-image");
+const imageCloseButton = content.querySelector(".popout-image__button-close");
+
+const overlayPopout = content.querySelector("#overlay");
+
+const cardTemplate = document.querySelector("#card").content;
+const initialCards = [
+  {
+    name: "Valle de Yosemite",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/yosemite.jpg",
+  },
+  {
+    name: "Lago Louise",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/lake-louise.jpg",
+  },
+  {
+    name: "Montañas Calvas",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/bald-mountains.jpg",
+  },
+  {
+    name: "Latemar",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/latemar.jpg",
+  },
+  {
+    name: "Parque Nacional de la Vanoise",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/vanoise.jpg",
+  },
+  {
+    name: "Lago di Braies",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/lago.jpg",
+  },
+];
+
+function createCard(card) {
+  const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+
+  cardElement.querySelector(".card__image").src = card.link;
+  cardElement.querySelector(".card__image").alt = card.name;
+  cardElement.querySelector(".card__title").textContent = card.name;
+
+  cardElement
+    .querySelector(".card__button-like")
+    .addEventListener("click", () => activeLike(cardElement));
+
+  cardElement
+    .querySelector(".card__button-trash")
+    .addEventListener("click", () => trashCard(cardElement));
+
+  cardElement
+    .querySelector(".card__image")
+    .addEventListener("click", () => zoomImage(cardElement));
+
+  return cardElement;
+}
+
+initialCards.forEach((card) => {
+  const cardElement = createCard(card);
+  document.querySelector(".cards").append(cardElement);
+});
+
+function zoomImage(cardElement) {
+  imagePopout.classList.add("active");
+  overlayPopout.classList.add("active");
+
+  document.querySelector(".popout-image__image").src =
+    cardElement.querySelector(".card__image").src;
+  document.querySelector(".popout-image__image").alt =
+    cardElement.querySelector(".card__title");
+  document.querySelector(".popout-image__title").textContent =
+    cardElement.querySelector(".card__title").textContent;
+}
+
+function trashCard(cardElement) {
+  const buttonTrash = cardElement.querySelector("#button-trash");
+  cardElement.remove(buttonTrash);
+}
+
+function activeLike(cardElement) {
+  const buttonLike = cardElement.querySelector(".card__button-like");
+  if (buttonLike.classList.contains("active")) {
+    buttonLike.classList.remove("active");
+  } else {
+    buttonLike.classList.add("active");
+  }
+}
 
 function renderEdit() {
-  editPopout.classList.add("active");
+  editFormPopout.classList.add("active");
   overlayPopout.classList.add("active");
 }
 
-function closeEdit() {
-  editPopout.classList.remove("active");
-  overlayPopout.classList.remove("active");
+function renderAdd() {
+  addFormPopout.classList.add("active");
+  overlayPopout.classList.add("active");
 }
 
-function handleProfileFormSubmit(evt) {
+function handleEditFormSubmit(evt) {
   evt.preventDefault();
-  let nameInput = formElement.querySelector(".popout__form-name");
-  let aboutInput = formElement.querySelector(".popout__form-about");
+  const nameInput = editFormElement.querySelector(".popout-edit__form-name");
+  const aboutInput = editFormElement.querySelector(".popout-edit__form-text");
 
   const profileName = document.querySelector(".profile__info-name");
   const profileSubtitle = document.querySelector(".profile__info-subtitle");
@@ -27,10 +119,38 @@ function handleProfileFormSubmit(evt) {
   profileName.textContent = nameInput.value;
   profileSubtitle.textContent = aboutInput.value;
 
-  saveButton.addEventListener("click", closeEdit);
+  editSaveButton.addEventListener("click", closePopout);
+}
+
+function handleAddFormSubmit(evt) {
+  evt.preventDefault();
+  const titleInput = addFormElement.querySelector(".popout-add__form-title");
+  const urlInput = addFormElement.querySelector(".popout-add__form-url");
+
+  card.name = titleInput.value;
+  card.link = urlInput.value;
+
+  const cardElement = createCard(card);
+  document.querySelector(".cards").prepend(cardElement);
+
+  addSaveButton.addEventListener("click", closePopout);
+}
+
+function closePopout() {
+  editFormPopout.classList.remove("active");
+  addFormPopout.classList.remove("active");
+  imagePopout.classList.remove("active");
+  overlayPopout.classList.remove("active");
 }
 
 editButton.addEventListener("click", renderEdit);
-closeButton.addEventListener("click", closeEdit);
-formElement.addEventListener("submit", handleProfileFormSubmit);
-saveButton.addEventListener("click", closeEdit);
+editFormElement.addEventListener("submit", (evt) => handleEditFormSubmit(evt));
+editCloseButton.addEventListener("click", closePopout);
+editSaveButton.addEventListener("click", closePopout);
+
+addButton.addEventListener("click", renderAdd);
+addFormElement.addEventListener("submit", (evt) => handleAddFormSubmit(evt));
+addCloseButton.addEventListener("click", closePopout);
+addSaveButton.addEventListener("click", closePopout);
+
+imageCloseButton.addEventListener("click", closePopout);
